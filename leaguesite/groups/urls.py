e77b13d146ea69c66c43861670006ab3c41ae755
thejayadad <74875595+thejayadad@users.noreply.github.com>
2023-04-30 14:16:34 -0400

    update

--- leaguesite/groups/urls.py
+++ leaguesite/groups/urls.py
@@ -6,9 +6,9 @@ from . import views
 app_name = 'groups'
 
 urlpatterns = [
-    path('', views.ListGroups.as_view(), name='all'),
-    path('new/', views.CreateGroup.as_view(), name='create'),
-    path('posts/in/(?p<slug>[-\w]+/)/', views.SingleGroup.as_view(), name='single'),
-    path('join/(?p<slug>[-\w]+/)/', views.JoinGroup.as_view(), name='join'),
-    path('leave/(?p<slug>[-\w]+/)/', views.LeaveGroup.as_view(), name='leave')
+    path("", views.ListGroups.as_view(), name="all"),
+    path("new/", views.CreateGroup.as_view(), name="create"),
+    path("posts/in/<slug:slug>/", views.SingleGroup.as_view(), name="single"),
+    path("join/<slug:slug>/", views.JoinGroup.as_view(), name="join"),
+    path("leave/<slug:slug>/", views.LeaveGroup.as_view(), name="leave")
 ],
