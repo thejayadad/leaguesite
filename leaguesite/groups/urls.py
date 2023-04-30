f8a28e71bfae318b7d855c25eff6e5212576f5bb
thejayadad <74875595+thejayadad@users.noreply.github.com>
2023-04-30 12:14:20 -0400

    join & leave group

--- leaguesite/groups/urls.py
+++ leaguesite/groups/urls.py
@@ -8,5 +8,7 @@ app_name = 'groups'
 urlpatterns = [
     path('', views.ListGroups.as_view(), name='all'),
     path('new/', views.CreateGroup.as_view(), name='create'),
-    path('posts/in/(?p<slug>[-\w]+/)', views.SingleGroup.as_view(), name='single'),
-]
+    path('posts/in/(?p<slug>[-\w]+/)/', views.SingleGroup.as_view(), name='single'),
+    path('join/(?p<slug>[-\w]+/)/', views.JoinGroup.as_view(), name='join'),
+    path('leave/(?p<slug>[-\w]+/)/', views.LeaveGroup.as_view(), name='leave')
+],
--- leaguesite/groups/views.py
+++ leaguesite/groups/views.py
@@ -4,9 +4,65 @@ from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 
 from django.urls import reverse
-
+from django.shortcuts import get_object_or_404
 
 from django.views import generic 
+from groups.models import Group, GroupMember
+from django.contrib import messages
 
 class CreateGroup(LoginRequiredMixin, generic.CreateView):
     fields = ('name', 'description')
+    model = Group
+
+
+class SingleGroup(generic.DeleteView):
+    model = Group
+
+class ListGroups(generic.ListView):
+    model = Group
+
+
+class JoinGroup(LoginRequiredMixin,generic.RedirectView):
+    def get_redirect_url(self,*args,**kwargs):
+        return reverse('groups:single', kwargs={'slug': self.kwargs.get('slug')})
+    
+    def get(self,request,*args,**kwargs):
+        group = get_object_or_404(Group, slug=self.kwargs.get('slug'))
+
+        try:
+            GroupMember.objects.create(user=self.request.user,group=group)
+        
+        except:
+            messages.warning(self.request,"Warning already amemeber")
+        else:
+            messages.success(self.request, "You are now a member!")
+
+        return super().get(request,*args,**kwargs)
+
+
+class LeaveGroup(LoginRequiredMixin, generic.RedirectView):
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse("groups:single",kwargs={"slug": self.kwargs.get("slug")})
+
+    def get(self, request, *args, **kwargs):
+        models = Group
+
+        try:
+
+            membership = models.GroupMember.objects.filter(
+                user=self.request.user,
+                group__slug=self.kwargs.get("slug")
+            ).get()
+
+        except models.GroupMember.DoesNotExist:
+            messages.warning(
+                self.request,
+                "You can't leave this group because you aren't in it."
+            )
+        else:
+            membership.delete()
+            messages.success(
+                self.request,
+                "You have successfully left this group."
+            )
+        return super().get(request, *args, **kwargs)
